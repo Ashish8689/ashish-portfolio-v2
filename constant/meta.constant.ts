0ed173811b27e373092a2df1566dd4583d4d7116
Ashish Gupta <ashish@getcollate.io>
2023-08-05 00:24:57 +0530

    added favicon and meta details

---
+++ constant/meta.constant.ts
@@ -0,0 +1,4 @@
+export const HOST_PORT = 'https://theashishgupta.netlify.app/'
+
+export const META_KEYWORDS =
+    'developer, web developer, frontend developer, software engineer, software developer, freelancer, ashish, ashish gupta'
